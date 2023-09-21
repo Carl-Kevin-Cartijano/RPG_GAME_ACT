c16fa7661b00731afa0b3c8449cb8864a52369d2
Kaneki_Ken 05 <ccartijano@gmail.com>
2023-09-22 06:20:54 +0800

    Initial

--- Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/QuotesActivity.kt
+++ Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/QuotesActivity.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
@@ -51,6 +52,7 @@ class QuotesActivity : AppCompatActivity() {
             val quote = randomQuote
             if (quote.isNotEmpty()) {
                 saveQuoteToSharedPreferences(quoteType, quote)
+                Toast.makeText(this, "Quote Saved.", Toast.LENGTH_SHORT).show()
             }
         }
     }
