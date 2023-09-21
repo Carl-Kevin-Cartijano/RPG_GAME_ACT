00745a9973b767833b0de3eebeb3944a2e47408f
Kaneki_Ken 05 <ccartijano@gmail.com>
2023-09-22 05:16:49 +0800

    Initial

--- Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/FavoritesActivity.kt
+++ Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/FavoritesActivity.kt
@@ -2,51 +2,98 @@ package com.example.quotesrandomizer
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class FavoritesActivity : AppCompatActivity() {
-    private var favoritesTextView: TextView? = null
-    private var removeButton: Button? = null
-    private var sharedPreferences: SharedPreferences? = null
+
+    private lateinit var listView: ListView
+    private lateinit var deleteButton: Button
+
+    private val favoriteQuotes = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
-        favoritesTextView = findViewById(R.id.favoritesTextView)
-        removeButton = findViewById(R.id.removeButton)
-        sharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
-        displaySavedQuotes()
 
-        removeButton?.setOnClickListener {
-            // Call removeQuote with the desired quote type
-            removeQuote("Motivational") // Change "Motivational" to the desired quote type
+        listView = findViewById(R.id.listView)
+        deleteButton = findViewById(R.id.deleteButton)
+
+        // Retrieve saved quotes from SharedPreferences
+        val sharedPreferences: SharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
+
+        // Replace "Motivational", "Funny", and "Love" with the quote types you have
+        val quoteTypes = arrayOf("Motivational", "Funny", "Love")
+
+        for (quoteType in quoteTypes) {
+            val key = "$quoteType" + "_quote"
+            val quote = sharedPreferences.getString(key, null)
+            if (quote != null) {
+                favoriteQuotes.add("$quoteType Quote: $quote")
+            }
+        }
+
+        // Create an ArrayAdapter to display the saved quotes in a ListView
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, favoriteQuotes)
+
+        // Set the adapter for the ListView
+        listView.adapter = adapter
+
+        // Set click listeners for the ListView items
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedQuote = favoriteQuotes[position]
+            showDeleteConfirmationDialog(selectedQuote)
+        }
+
+        deleteButton.setOnClickListener {
+            // Check if a quote is selected for deletion
+            if (favoriteQuotes.isNotEmpty()) {
+                val selectedQuote = favoriteQuotes[0] // Delete the first quote
+                showDeleteConfirmationDialog(selectedQuote)
+            } else {
+                Toast.makeText(this, "No quotes available to delete.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    private fun removeQuote(quoteTypeToRemove: String) {
-        // Construct the key for the quote based on the quote type
-        val keyToRemove = "${quoteTypeToRemove}_quote"
+    private fun showDeleteConfirmationDialog(quote: String) {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Confirm Delete")
+        alertDialog.setMessage("Are you sure you want to delete this quote?")
+        alertDialog.setPositiveButton("Delete") { _, _ ->
+            // Delete the selected quote from SharedPreferences
+            val sharedPreferences: SharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
 
-        // Remove the quote from SharedPreferences using the constructed key
-        sharedPreferences?.edit()?.remove(keyToRemove)?.apply()
+            // Determine the quote type from the selectedQuote and remove it
+            if (quote.startsWith("Motivational Quote")) {
+                editor.remove("Motivational_quote")
+            } else if (quote.startsWith("Funny Quote")) {
+                editor.remove("Funny_quote")
+            } else if (quote.startsWith("Love Quote")) {
+                editor.remove("Love_quote")
+            }
 
-        // Update the displayed quotes
-        displaySavedQuotes()
+            editor.apply()
+
+            // Remove the deleted quote from the list and refresh the ListView
+            favoriteQuotes.remove(quote)
+            refreshListView()
+            Toast.makeText(this, "Quote deleted.", Toast.LENGTH_SHORT).show()
+        }
+        alertDialog.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+        alertDialog.show()
     }
 
-    private fun displaySavedQuotes() {
-        // Retrieve all saved quotes from SharedPreferences and display them
-        val allEntries = sharedPreferences?.all
-        val quotesBuilder = StringBuilder()
-
-        allEntries?.forEach { (key, value) ->
-            val quoteType = key.split("_")[0]
-            val quote = value.toString()
-            quotesBuilder.append("$quoteType: $quote\n")
-        }
-
-        favoritesTextView?.text = quotesBuilder.toString()
+    private fun refreshListView() {
+        // Refresh the ListView with the updated quote list after deletion
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, favoriteQuotes)
+        listView.adapter = adapter
     }
 }
--- Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/QuotesActivity.kt
+++ Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/QuotesActivity.kt
@@ -1,36 +1,54 @@
 package com.example.quotesrandomizer
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class QuotesActivity : AppCompatActivity() {
-    private var quoteTextView: TextView? = null
-    private var saveButton: Button? = null
+    private lateinit var quoteTextView: TextView
+    private lateinit var saveButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
-        quoteTextView = findViewById<TextView>(R.id.quoteTextView)
-        saveButton = findViewById<Button>(R.id.saveButton)
+
+        quoteTextView = findViewById(R.id.quoteTextView)
+        saveButton = findViewById(R.id.saveButton)
+
         val quoteType = intent.getStringExtra("quoteType")
 
-        // Implement logic to fetch and display quotes of the selected type
-        @Suppress("ImplicitThis")
-        saveButton.setOnClickListener(View.OnClickListener {
-            saveQuoteToSharedPreferences(
-                quoteType,
-                with(quoteTextView) { this?.getText().toString() }
-            )
-        })
+        // Define an array of quotes for the selected quote type
+        val quotes = when (quoteType) {
+            "Motivational" -> arrayOf("When you have a dream, you've got to grab it and never let go.", "Nothing is impossible. The word itself says 'I'm possible!'", "There is nothing impossible to they who will try.")
+            "Funny" -> arrayOf("There is no sunrise so beautiful that it is worth waking me up to see it.", "Life is short. Drive fast and leave a sexy corpse. That's one of my mottos.", "I always cook with wine. Sometimes I even add it to the food.")
+            "Love" -> arrayOf("Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.", "What we have once enjoyed we can never lose. All that we love deeply becomes a part of us.", "Of all forms of caution, caution in love is perhaps the most fatal to true happiness.")
+            else -> arrayOf()
+        }
+
+        // Randomly select a quote from the array
+        val randomIndex = Random.nextInt(quotes.size)
+        val randomQuote = quotes[randomIndex]
+
+        // Display the random quote
+        quoteTextView.text = randomQuote
+
+        saveButton.setOnClickListener {
+            val quote = randomQuote
+            if (quote.isNotEmpty()) {
+                saveQuoteToSharedPreferences(quoteType, quote)
+            }
+        }
     }
 
     private fun saveQuoteToSharedPreferences(quoteType: String?, quote: String) {
-        val sharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val key = quoteType + "_quote"
+        val sharedPreferences: SharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        val key = "$quoteType" + "_quote"
         editor.putString(key, quote)
         editor.apply()
+
     }
 }
