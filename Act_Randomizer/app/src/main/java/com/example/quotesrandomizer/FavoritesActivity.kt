dc7f3ce4b6d277123283b80d06bd237ddd6f9607
Kaneki_Ken 05 <ccartijano@gmail.com>
2023-09-22 06:04:37 +0800

    Initial

--- Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/FavoritesActivity.kt
+++ Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/FavoritesActivity.kt
@@ -8,13 +8,18 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class FavoritesActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
     private lateinit var deleteButton: Button
 
-    private val favoriteQuotes = mutableListOf<String>()
+    private val favoriteQuotes = mutableListOf<QuoteWithTimestamp>()
+
+    data class QuoteWithTimestamp(val quote: String, val timestamp: Long)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,26 +28,30 @@ class FavoritesActivity : AppCompatActivity() {
         listView = findViewById(R.id.listView)
         deleteButton = findViewById(R.id.deleteButton)
 
-
         val sharedPreferences: SharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
-
 
         val quoteTypes = arrayOf("Motivational", "Funny", "Love")
 
         for (quoteType in quoteTypes) {
             val key = "$quoteType" + "_quote"
             val quote = sharedPreferences.getString(key, null)
+            val timestamp = sharedPreferences.getLong(key + "_timestamp", 0)
+
             if (quote != null) {
-                favoriteQuotes.add("$quoteType Quote: $quote")
+                val formattedTimestamp = formatTimestamp(timestamp)
+                val quoteWithTimestamp = "$quoteType Quote: $quote\nSaved on: $formattedTimestamp"
+                favoriteQuotes.add(QuoteWithTimestamp(quoteWithTimestamp, timestamp))
             }
         }
 
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, favoriteQuotes)
-
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.list_item_with_timestamp, // Use the new layout with timestamps
+            R.id.quoteTextView, // Use the correct TextView ID for the quote text
+            favoriteQuotes.map { it.quote }
+        )
 
         listView.adapter = adapter
-
 
         listView.setOnItemClickListener { _, _, position, _ ->
             val selectedQuote = favoriteQuotes[position]
@@ -50,7 +59,6 @@ class FavoritesActivity : AppCompatActivity() {
         }
 
         deleteButton.setOnClickListener {
-
             if (favoriteQuotes.isNotEmpty()) {
                 val selectedQuote = favoriteQuotes[0] // Delete the first quote
                 showDeleteConfirmationDialog(selectedQuote)
@@ -60,23 +68,19 @@ class FavoritesActivity : AppCompatActivity() {
         }
     }
 
-    private fun showDeleteConfirmationDialog(quote: String) {
+    private fun showDeleteConfirmationDialog(quote: QuoteWithTimestamp) {
         val alertDialog = AlertDialog.Builder(this)
         alertDialog.setTitle("Confirm Delete")
         alertDialog.setMessage("Are you sure you want to delete this quote?")
         alertDialog.setPositiveButton("Delete") { _, _ ->
-
             val sharedPreferences: SharedPreferences = getSharedPreferences("MyQuotes", MODE_PRIVATE)
             val editor: SharedPreferences.Editor = sharedPreferences.edit()
 
-            if (quote.startsWith("Motivational Quote")) {
-                editor.remove("Motivational_quote")
-            } else if (quote.startsWith("Funny Quote")) {
-                editor.remove("Funny_quote")
-            } else if (quote.startsWith("Love Quote")) {
-                editor.remove("Love_quote")
-            }
+            val quoteType = extractQuoteType(quote.quote)
+            val key = "$quoteType" + "_quote"
 
+            editor.remove(key)
+            editor.remove(key + "_timestamp")
             editor.apply()
 
             favoriteQuotes.remove(quote)
@@ -90,7 +94,26 @@ class FavoritesActivity : AppCompatActivity() {
     }
 
     private fun refreshListView() {
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, favoriteQuotes)
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.list_item_with_timestamp, // Use the new layout with timestamps
+            R.id.quoteTextView, // Use the correct TextView ID for the quote text
+            favoriteQuotes.map { it.quote }
+        )
         listView.adapter = adapter
+    }
+
+    private fun formatTimestamp(timestamp: Long): String {
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return sdf.format(Date(timestamp))
+    }
+
+    private fun extractQuoteType(quote: String): String {
+        return when {
+            quote.startsWith("Motivational Quote") -> "Motivational"
+            quote.startsWith("Funny Quote") -> "Funny"
+            quote.startsWith("Love Quote") -> "Love"
+            else -> ""
+        }
     }
 }
--- Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/QuotesActivity.kt
+++ Act_Randomizer/app/src/main/java/com/example/quotesrandomizer/QuotesActivity.kt
@@ -22,9 +22,21 @@ class QuotesActivity : AppCompatActivity() {
 
 
         val quotes = when (quoteType) {
-            "Motivational" -> arrayOf("When you have a dream, you've got to grab it and never let go.", "Nothing is impossible. The word itself says 'I'm possible!'", "There is nothing impossible to they who will try.")
-            "Funny" -> arrayOf("There is no sunrise so beautiful that it is worth waking me up to see it.", "Life is short. Drive fast and leave a sexy corpse. That's one of my mottos.", "I always cook with wine. Sometimes I even add it to the food.")
-            "Love" -> arrayOf("Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.", "What we have once enjoyed we can never lose. All that we love deeply becomes a part of us.", "Of all forms of caution, caution in love is perhaps the most fatal to true happiness.")
+            "Motivational" -> arrayOf(  "When you have a dream, you've got to grab it and never let go.",
+                                        "Nothing is impossible. The word itself says 'I'm possible!'",
+                                        "There is nothing impossible to they who will try.",
+                                        "The bad news is time flies. The good news is you're the pilot.",
+                                        "Life has got all those twists and turns. You've got to hold on tight and off you go.")
+            "Funny" -> arrayOf( "There is no sunrise so beautiful that it is worth waking me up to see it.",
+                                "Life is short. Drive fast and leave a sexy corpse. That's one of my mottos.",
+                                "I always cook with wine. Sometimes I even add it to the food.",
+                                "People say money is not the key to happiness, but I have always figured if you have enough money, you can have a key made.",
+                                "Do not take life too seriously. You will never get out of it alive.")
+            "Love" -> arrayOf(  "Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.",
+                                "What we have once enjoyed we can never lose. All that we love deeply becomes a part of us.",
+                                "Of all forms of caution, caution in love is perhaps the most fatal to true happiness.",
+                                "Loved you yesterday, love you still, always have, always will.",
+                                "To love well is the task in all meaningful relationships, not just romantic bonds.")
             else -> arrayOf()
         }
 
